a61856d262a80645bb909d93916dc97c5edefdc3
Satyam <satyam482002@gmail.com>
2023-02-21 17:05:24 +0530

    Updates

---
+++ precache-manifest.dcf1c0ae002e788109775a8cfd46d9a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "33f94af59e3acf2f89f8",
+    "url": "/quiz-app/static/css/main.c818009d.chunk.css"
+  },
+  {
+    "revision": "33f94af59e3acf2f89f8",
+    "url": "/quiz-app/static/js/main.33f94af5.chunk.js"
+  },
+  {
+    "revision": "e52a0f2710e05d1c8602",
+    "url": "/quiz-app/static/js/1.e52a0f27.chunk.js"
+  },
+  {
+    "revision": "b00fbc1d210523beecb4",
+    "url": "/quiz-app/static/js/2.b00fbc1d.chunk.js"
+  },
+  {
+    "revision": "d3e5414bc279c7189825",
+    "url": "/quiz-app/static/js/runtime~main.d3e5414b.js"
+  },
+  {
+    "revision": "e55862ad3acb9ac8ac4454221366f389",
+    "url": "/quiz-app/static/media/wrong.e55862ad.mp3"
+  },
+  {
+    "revision": "d27f45adf88f4e70e787423e4db695ad",
+    "url": "/quiz-app/static/media/correct.d27f45ad.mp3"
+  },
+  {
+    "revision": "3d8ba0a6804c784263ebab21dc1f4027",
+    "url": "/quiz-app/static/media/play.3d8ba0a6.mp3"
+  },
+  {
+    "revision": "889b8ea9616d69c19dbd3754aea454e5",
+    "url": "/quiz-app/static/media/bg.889b8ea9.jpg"
+  },
+  {
+    "revision": "d50e4ee359aa08b50e1dedc5f9d8e049",
+    "url": "/quiz-app/index.html"
+  }
+];
